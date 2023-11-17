d1c4e96fb8458e0ce337a772e0df452409a900a0
erp <frappe@N7N0LUJD83JKS>
2023-11-17 11:44:40 +0700

    update

--- addons/addons/doctype/parent_doc/parent_doc.py
+++ addons/addons/doctype/parent_doc/parent_doc.py
@@ -2,9 +2,22 @@
 # For license information, please see license.txt
 
 import frappe
+from frappe import _
 from frappe.model.document import Document
 
-class ParentDoc(Document):
+class ParentDoc(Document):	
 	
 	def validate(self):
-		frappe.msgprint("Halo dari server script")
+		self.check_data()
+                
+				
+	def check_data(self):
+		if self.enable:
+			if self.workflow_state=="Waiting SPV":
+				# for data in self.date_and_value:
+				# 	frappe.throw(_("Gagal update"))
+				# for data in self.date_and_value:
+				# 	print(data)
+				frappe.throw("dd")
+			
+		
---
+++ addons/addons/doctype/parent_doc/parent_doc_list.js
@@ -0,0 +1,5 @@
+frappe.listview_settings['Parent Doc'] = {
+	// onload: function (doclist) {
+    //     console.log(doclist)
+	// }
+};
